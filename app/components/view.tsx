080c2bc8e18926444671331f86f78d715f2cb4eb
Avinash Pathak <avipathak732@gmail.com>
2025-05-31 20:42:41 +0530

    write-client to update dynamic views via ppr

--- app/components/view.tsx
+++ app/components/view.tsx
@@ -2,9 +2,13 @@ import React from "react"
 import Ping from "./Ping"
 import { client } from "@/sanity/lib/client"
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries"
+import { writeClient } from "@/sanity/lib/write-client"
 
 export default async function View ({id}:{id: string}){
     const {views: totalViews} = await client.withConfig({useCdn: false}).fetch(STARTUP_VIEWS_QUERY, {id})
+
+    await writeClient.patch(id).set({views: totalViews+1}).commit()
+
     return (
         <div className="view-container flex justify-end items-center mt-5 fixed bottom-3 right-3">
             <div 
