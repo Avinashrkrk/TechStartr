dc1eafc86cbc92a55dde6a35d935a3388188ed66
Avinash Pathak <avipathak732@gmail.com>
2025-06-02 13:25:29 +0530

    session id and  authentication flow

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -46,3 +46,15 @@ export const STARTUP_VIEWS_QUERY = defineQuery(`
     _id, views
   } 
 `)
+
+export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`
+  *[_type == 'author' && id == $id][0]{ // Change _id == $id to id == $id
+  _id,
+  id,
+  name,
+  username,
+  email,
+  image,
+  bio
+}
+`)
